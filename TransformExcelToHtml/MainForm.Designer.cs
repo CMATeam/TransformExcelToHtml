ed64e15722415c86694dc1b7381953b2bdd2ff27
Do Trung Duc <duc010298@gmail.com>
2020-02-29 21:21:36 +0700

    Transform one sheet

---
+++ TransformExcelToHtml/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TransformExcelToHtml
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose excel file";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(242, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(323, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Scan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 481);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Transform Excel to Html";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TransformExcelToHtml/MainForm.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TransformExcelToHtml
+{
+    public partial class MainForm : Form
+    {
+        private string excelFilePath;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            excelFilePath = openFileDialog1.FileName;
+            textBox1.Text = openFileDialog1.SafeFileName;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var excelApp = new Excel.Application();
+            // Make the object visible.
+            excelApp.Visible = true;
+            Workbook wb = excelApp.Workbooks.Open(excelFilePath);
+
+            // Loop ws throught wb
+            IEnumerator wsEnumerator = excelApp.ActiveWorkbook.Worksheets.GetEnumerator();
+            object missing = Type.Missing;
+            object format = Microsoft.Office.Interop.Excel.XlFileFormat.xlHtml;
+            //while (wsEnumerator.MoveNext())
+            //{
+                Workbook wsCurrent = wb;
+                String outputFile = excelFilePath + ".html";
+                //wsCurrent.SaveAs(outputFile, format, missing, missing, missing,
+                //    missing, XlSaveAsAccessMode.xlNoChange, missing, missing, missing, missing, missing);
+                PublishObjects publish = wsCurrent.PublishObjects;
+                System.Diagnostics.Debug.WriteLine("outputFile: " + outputFile);
+                System.Diagnostics.Debug.WriteLine("XlSourceType.xlSourcePrintArea: " + XlSourceType.xlSourcePrintArea);
+                publish.Add(
+                    XlSourceType.xlSourcePrintArea,
+                    outputFile,
+                    "default",
+                    XlSourceType.xlSourcePrintArea,
+                    XlHtmlType.xlHtmlStatic,
+                    "",
+                    ""
+                ).Publish(true);
+            //}
+            //excelApp.Quit();
+            MessageBox.Show("Done", "TETH", MessageBoxButtons.OK);
+        }
+    }
+}
